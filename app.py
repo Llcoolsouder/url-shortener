a573eb26209d82d26cb1583ecb6d51cf7691605d
Lonnie Souder II <lonnie2@souderfamily.net>
2020-11-24 17:35:54 -0500

    app makes database and table on startup and adds entries on POSTs to api

--- app.py
+++ app.py
@@ -1,29 +1,55 @@
+import sqlite3
+import hashlib
 from flask import (
     Flask,
-    Blueprint
+    Blueprint,
+    url_for,
+    request
 )
+
 
 app = Flask('url-shortener')
 api = Blueprint('api', __name__)
+database = 'links.db'
+conn = sqlite3.connect(database)
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS links (
+        source text,
+        destination text)
+''')
+conn.commit()
+conn.close()
+
 
 @api.route('/shorten/', methods=['POST'])
 def shorten():
     '''
     Creates a shortened url that links to the url given via form
     '''
+    source = request.form['url']
+    destination_tag = hashlib.md5(source.encode('utf-8')).hexdigest()[0:8]
+    destination = url_for('api.short', tag=destination_tag)
+    connection = sqlite3.connect(database)
+    cursor = connection.cursor()
+    cursor.execute(f"INSERT INTO links VALUES ('{source}', '{destination_tag}')")
+    connection.commit()
+    connection.close()
+    return f'<a href={destination}>{destination}</a> now redirects to {source}'
+
+
+@api.route('/short/<tag>/')
+def short(tag):
+    '''
+    Redirects to the url previously associated with tag
+    Parameters:
+        tag (str): unique piece of shortened url
+    '''
     return 'This endpoint has not been implemented.'
 
-@api.route('/short/<url>/')
-def short(url):
-    '''
-    Redirects to the url previously associated with url
-    Parameters:
-        url (str): unique piece of shortened url
-    '''
-    return 'This endpoint has not been implemented.'
 
 app.register_blueprint(api, url_prefix='/api')
 
 
 if __name__ == '__main__':
-    app.run(debug = True) # TODO: Remove debug mode
+    app.run(debug=True)  # TODO: Remove debug mode
