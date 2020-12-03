93a0794683b15de2e68d21a7330119ee2f2bc412
Lonnie Souder II <lonnie2@souderfamily.net>
2020-12-03 17:50:37 -0500

    added react app to post requests to /api/shorten/

--- frontend-src/src/App.js
+++ frontend-src/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+
+class UrlShortener extends React.Component {
+  constructor(props) {
+    super(props);
+    this.API_URI = "http://localhost:80/api/shorten/";
+
+    this.state = {
+      url: "",
+    };
+
+    this.updateTextState = this.updateTextState.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch(this.API_URI, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(this.state),
+    })
+      .then((resp) => resp.json())
+      .then((json) => console.log(json));
+  }
+
+  updateTextState(event) {
+    this.setState({ url: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="url-shortener">
+        <h1>Url Shortener</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="url">Url to shorten:</label>
+          <input
+            type="text"
+            name="url"
+            id="url"
+            value={this.state.url}
+            onChange={this.updateTextState}
+            required
+          />
+          <button type="submit">Shorten!</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <UrlShortener />;
 }
 
 export default App;
